545e8e3e79c64ba7511c071920233ba80f3f1445
Jonas Friedemann Grote <jonas.grote@1und1.de>
2012-07-02 18:15:13 +0200

    Fixed test

--- buffer/tests/BufferTest.cpp
+++ buffer/tests/BufferTest.cpp
@@ -26,70 +26,60 @@ protected:
 };
 
 TEST_F(BufferTest, Write) {
-    cout << "hmm." << endl;
 //    char result[256];
     string line;
     char text[] = "mal gucken, was das tut.\" und so weiter.. babaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccddddddddddddddddddddddddddddddddddddddddddddddddddddddd\nZ =?= ((3 +++ 4 - 6));\nÄy=X / (X - 4);\nprinto read a <=>*b;\n(* eine einfache Aufgabe !! *)\nm = n <=/ o;\n";
-//    cout << "doing it soon" << endl;
     write_buffer->addchars(text);
-//    cout << "done" << endl;
     delete write_buffer;
-//    cout << "it is over" << endl;
 
     ifstream inFile("/tmp/buffertestfile2");
 
-    int count = 0, i;
+    unsigned int count = 0, i;
+    int done_with_text = 0;
+    int empty_chars = 0;
+
     if (inFile.is_open()) {
         while (inFile.good()) {
             getline(inFile, line);
             for (i=0; i < line.size(); i++) {
-                if (text[i+count] == '\0') {
-                    break;
+                if (text[count+i] == '\0') {
+                    done_with_text = 1;
                 }
-                ASSERT_EQ(text[i+count], line[i]) << "At count: " << count;
-//                cout << "checked char: " << text[i+count] << endl;
+                if (!done_with_text) {
+                    ASSERT_EQ(text[count+i], line[i]) << "At count, i: " << count << ", " << i;
+                } else {
+                    if (!inFile.good()) {
+                        ASSERT_EQ('\0', line[i]) << "At count, i: " << count << ", " << i;
+                        count--;
+                    } else {
+                        ASSERT_EQ(' ', line[i]) << "At count, i: " << count << ", " << i;
+                        empty_chars++;
+                    }
+                }
             }
-            count += i + 1; // +1 because of newline
+            if (text[count+i] == '\n') {
+                count += 1; // "line" doesn't have newline character
+            }
+            count += i;
         }
         inFile.close();
     } else {
         cout << "file not open!" << endl;
     }
 
-    /*
-    int i=0;
-    if (inFile.is_open()) {
-        cout << " starting reading " << endl;
-        while (inFile >> x) {
-            cout << " got char " << x << endl;
-            ASSERT_EQ(text[i], x) << "At i: " << i;
-            i++;
-        }
-        cout << " done " << endl;
-        inFile.close();
-    } else {
-        cerr << "file not open!" << endl;
-    }
-    */
-
-    ASSERT_EQ(strlen(text), count);
-
-//    for (int i=0; i < 20; i++) {
-//        cout << result[i] << endl;
-//    }
+    ASSERT_EQ(strlen(text) + empty_chars, count) << "Checking if file was checked at all";
 }
 
 
 TEST_F(BufferTest, Read) {
-    cout << "hmm2." << endl;
     char expected[] = "babaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccddddddddddddddddddddddddddddddddddddddddddddddddddddddd\nZ =?= ((3 +++ 4 - 6));\nÄy=X / (X - 4);\nprinto read a <=>*b;\n(* eine einfache Aufgabe !! *)\nm = n <=/ o;\n";
-//    cout << sizeof(expected) << endl;
 
+    unsigned int i;
     int len = strlen(expected);
-    for (int i=0; i < len * 8; i++) {
+    for (i=0; i < len * 8; i++) {
         char current_char = read_buffer->getchar();
         ASSERT_EQ(expected[i%len], current_char) << "At i: " << i;
-//        cout << current_char << endl;
     }
-    //cout << endl;
+
+    ASSERT_EQ(len*8, i) << "Number of checked characters needs to be length of text";
 }
